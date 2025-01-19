f66c37bdb59d6cef974f75e136fc764d934de94f
XE <XE@XE1179>
2025-01-19 15:18:31 +0300

    Добавлена основная программа, добавлены комментарии

--- GENIUS/Program.cs
+++ GENIUS/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string variable_string = "это строка";
+int variable_int = 12;
+double variable_double = 24,5;
+bool variable_bool = true;
+char variable_char = 'a';
+char variable_char2 = 'b';
+
+Console.WriteLine("Тип данных - строчка " + variable_string);
+Console.WriteLine("Тип данных - целочисленный " + variable_int);
+Console.WriteLine("Тип данных - символьный " + variable_char);
+Console.WriteLine("Тип данных - дробный " + variable_double);
+Console.WriteLine("Тип данных - логический " + variable_bool);
+
+Console.WriteLine(variable_int + variable_double);
+
+
+
+
+Console.WriteLine("Введите переменную типа int: ");
+int xInt;
+xInt = Int32.Parse(Console.ReadLine());
+Console.WriteLine(xInt + " - ваше значение. ");
+
+Console.WriteLine("Введите переменную типа char: ");
+char xChar;
+xChar = Char.Parse(Console.ReadLine());
+Console.WriteLine(xChar + " - ваше значение. ");
+
+Console.WriteLine("Введите переменную типа bool: ");
+bool xBool;
+xBool = Boolean.Parse(Console.ReadLine());
+Console.WriteLine(xBool + " - ваше значение. ");
+
+Console.WriteLine("Введите переменную типа double: ");
+double xDouble;
+xDouble = Double.Parse(Console.ReadLine());
+Console.WriteLine(xDouble + " - ваше значение. ");
